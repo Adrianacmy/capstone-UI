b08e293a63d017c7cddb3b7bf5e672be29f4cbb1
Adrianacmy <iamadrianachen@gmail.com>
2018-07-18 11:09:11 -0700

    add responsive searchform

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,38 +1,77 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
-import {
-  Navbar,
-  NavItem,
-  Dropdown,
-  Icon,
-  Modal,
-  Button
-} from 'react-materialize';
+import SearchForm from '../search/search_form';
+import { SideNavItem, SideNav } from 'react-materialize';
+import m from './m.jpg';
+import me from '../../assets/images/me.jpg';
+
+import { Navbar, NavItem, Modal } from 'react-materialize';
 
 export default () => {
+  console.log(me);
   return (
-    <Navbar brand="logo" right>
-      <NavItem>
-        <Modal
-          header="Search Anything"
-          trigger={<i className="material-icons">search</i>}
-        >
-          <div className="nav-wrapper">
-            <form>
-              <div className="input-field">
-                <input id="search" type="search" required />
-                <label className="label-icon" for="search">
-                  <i className="material-icons">search</i>
-                </label>
-                <i className="material-icons">close</i>
+    <div className="purple">
+      <Navbar brand="enBazaar" right className="container purple z-depth-0">
+        <SideNavItem
+          userView
+          user={{
+            image: '/static/media/me.d3106e79.jpg',
+            name: 'Bonnie Shen',
+            email: 'Bonniek@gmail.com'
+          }}
+          className="hide-on-med-and-up purple"
+        />
+
+        <NavItem className="hide-on-med-and-up">
+          <Modal
+            header="Search Anything"
+            trigger={<i className="material-icons">search</i>}
+          >
+            <div className="nav-wrapper">
+              <SearchForm />
+            </div>
+          </Modal>
+        </NavItem>
+        <NavItem devider className="grey hide-on-med-and-up">
+          <i className="material-icons">local_florist</i>Portfolio
+        </NavItem>
+        <SideNavItem href="#!icon" className="hide-on-med-and-up">
+          <i className="material-icons">nature</i>Home
+        </SideNavItem>
+        <SideNavItem href="#!second" className="hide-on-med-and-up">
+          <i className="fas fa-external-link" />News
+        </SideNavItem>
+        <SideNavItem subheader className="hide-on-med-and-up">
+          <i className="material-icons">local_florist</i>Price
+        </SideNavItem>
+        <SideNavItem waves href="#!third" className="hide-on-med-and-up">
+          <i className="material-icons">local_florist</i>History
+        </SideNavItem>
+
+        <NavItem href="#" className="hide-on-med-and-up">
+          <i class="fas fa-cog" />Setting
+        </NavItem>
+        <NavItem href="components.html" className="hide-on-med-and-up">
+          <i class="fas fa-question" />Help
+        </NavItem>
+
+        <div className="hide-on-med-and-down right">
+          <NavItem>
+            <Modal
+              header="Search Anything"
+              trigger={<i className="material-icons">search</i>}
+            >
+              <div className="nav-wrapper">
+                <SearchForm style={{ width: '100vw' }} />
               </div>
-            </form>
-          </div>
-        </Modal>
-      </NavItem>
-      <NavItem onClick={() => console.log('test click')}>enBazaar</NavItem>
-      <NavItem href="components.html">Components</NavItem>
-    </Navbar>
+            </Modal>
+          </NavItem>
+          <NavItem>Home</NavItem>
+          <NavItem>News</NavItem>
+          <NavItem>Account</NavItem>
+        </div>
+      </Navbar>
+    </div>
   );
 };
---
+++ src/components/search/search_form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <form>
+      <div className="input-field col s12">
+        <input id="search" type="search" required />
+        <label className="label-icon" for="search">
+          <i className="material-icons">search</i>
+        </label>
+        <i className="material-icons">close</i>
+      </div>
+    </form>
+  );
+};
