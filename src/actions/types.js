16893ba2c14606cb935d2807e10f6ee7ad0c84c0
Adrianacmy <iamadrianachen@gmail.com>
2018-07-23 11:03:30 -0700

    add data to component topMovers

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,3 +2,4 @@ export const AUTH_USER = 'auth_user';
 export const AUTH_ERROR = 'auth_error';
 export const RECENT_NEWS = 'recent_news';
 export const TOP_MOVERS = 'top_movers';
+// export const FETCH_CHANGES = 'top_movers';
--- src/components/top_movers/TopMovers.js
+++ src/components/top_movers/TopMovers.js
@@ -3,101 +3,47 @@ import welcome from '../../assets/images/welcome.png';
 
 import { Col, Card } from 'react-materialize';
 
-export default class App extends React.Component {
-  state = {
-    changes: []
-  };
-
-  //   async componentDidMount() {
-  //     const res = await fetch(
-  //       `https://api.coinmarketcap.com/v1/ticker/${symbol}/`
-  //     );
-  //     const data = await res.json();
-
-  //     this.setState({
-  //       // return array of ojbect
-  //       data: Object.keys(data.bpi).map(date => {
-  //         return {
-  //           date,
-  //           price: data.bpi[date]
-  //         };
-  //       })
-  //     });
-  //   }
-  // }
-  // props => {
-  //   console.log(props);
-  //   let movers = [];
-  //   props.topMovers.map(mover => {
-  //     movers.push(mover.CoinInfo.FullName);
-  //   });
-  //   // console.log(movers);
-  //   async function fetchChanges(symble) {
-  //     const res = await fetch(
-  //       `https://api.coinmarketcap.com/v1/ticker/bitcoine/`
-  //     );
-  //     const data = await res.json();
-  //     this.setState({});
-  //   }
-  render() {
-    return (
-      <div id="top-movers" className="hide-on-small-and-down">
-        <div className="title-more">
-          <h4>Top Movers</h4>
-          <span>
-            {' '}
-            <a className="btn-floating btn-medium waves-effect waves-light indigo lighten-3">
-              <i className="material-icons">more_horiz</i>
-            </a>
-          </span>
-        </div>
-        <div className="card-box">
-          <Col m={3} s={6}>
-            <Card
-              className="grey lighten-5 hoverable"
-              textClassName="grey-text"
-              title="Card title"
-              actions={[<a href="#">This is a link</a>]}
-              key="22231"
-            >
-              I am a very simple card.
-            </Card>
-          </Col>
-          <Col m={3} s={6}>
-            <Card
-              className="grey lighten-5 hoverable"
-              textClassName="grey-text"
-              title="Card title"
-              actions={[<a href="#">This is a link</a>]}
-              key="222531"
-            >
-              I am a very simple card.
-            </Card>
-          </Col>
-          <Col m={3} s={6}>
-            <Card
-              className="grey lighten-5 hoverable"
-              textClassName="grey-text"
-              title="Card title"
-              actions={[<a href="#">This is a link</a>]}
-              key="222315"
-            >
-              I am a very simple card.
-            </Card>
-          </Col>
-          <Col m={3} s={6}>
-            <Card
-              className="grey lighten-5 hoverable"
-              textClassName="grey-text"
-              title="Card title"
-              actions={[<a href="#">This is a link</a>]}
-              key="2223157"
-            >
-              I am a very simple card.
-            </Card>
-          </Col>
-        </div>
-      </div>
-    );
+export default props => {
+  let localMovers;
+  if (!props.topMovers) {
+    localMovers = ['bitcoin'];
+  } else {
+    localMovers = props.topMovers.slice(0, 4);
   }
-}
+  // console.log(localMovers)
+  return (
+    <div id="top-movers" className="hide-on-small-and-down">
+      <div className="title-more">
+        <h4>Most Volumes</h4>
+        <span>
+          {' '}
+          <a className="btn-floating btn-medium waves-effect waves-light indigo lighten-3">
+            <i className="material-icons">more_horiz</i>
+          </a>
+        </span>
+      </div>
+      <div className="card-box">
+        {localMovers.map(symbl => {
+          return (
+            <Col m={3} s={6}>
+              <Card
+                className="grey lighten-5 hoverable"
+                textClassName="grey-text"
+                title={symbl.CoinInfo.Name}
+                actions={[
+                  <a href={symbl.CoinInfo.url}>
+                    TotalVolume 24H:<br />{' '}
+                    {symbl.ConversionInfo.TotalVolume24H.toFixed(2)}
+                  </a>
+                ]}
+                key="22231"
+              >
+                {symbl.CoinInfo.FullName}
+              </Card>
+            </Col>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
